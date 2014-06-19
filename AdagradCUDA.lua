fc989bcee15f818225ec7e4667ea2c23e5a0f434
y0ast <joost@joo.st>
2014-06-19 17:15:08 +0200

    fix bugs gahhhh

--- AdagradCUDA.lua
+++ AdagradCUDA.lua
@@ -23,25 +23,21 @@ function adaGradUpdate(batch, N, learningRate, opfunc, h)
     local batchSize = batch:size(1)
     local weights, grads, lowerbound = opfunc(batch)
 
-	print(grads)
     for i=1,#h do
+        local hupdate = grads[i]:clone()
+        h[i]:add(hupdate:cmul(hupdate))
 
         local prior = weights[i].new()
-	prior:resizeAs(weights[i]):fill(0)
+    	prior:resizeAs(weights[i]):fill(0)
 
-        if i % 2 == 1 then
+        if i % 2 ~= 1 then
             prior:add(-0.5):cmul(weights[i]):mul(batchSize/N)
-	end
+    	end
 
-        local update = h[i].new()
-	update:resizeAs(h[i]):fill(learningRate)
-
-        update:cdiv(h[i]):cdiv(h[i]):cmul(prior:add(grads[i]))
-	print(torch.norm(update:double()))
+        local update = h[i]:clone()
+        update:pow(-0.5):mul(learningRate):cmul(prior:add(grads[i]))
 
         weights[i]:add(update)
-
-        h[i]:add(grads[i]:pow(2))
     end
 
     collectgarbage()
