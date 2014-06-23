9e1ec16bb4e61214d531d710e777454b2238687a
OttoFabius <ottofabius@gmail.com>
2014-06-23 15:03:40 +0200

    changed 14-7 and 7-7

--- results/MNIST/cuda/2-layer-14-7/config.lua
+++ results/MNIST/cuda/2-layer-14-7/config.lua
@@ -1,14 +1,23 @@
-batchSize = 100 -- size of mini-batches
+require 'cutorch'
+require 'cunn'
+require 'SpatialZeroPaddingCUDA'
+
+cuda = true
+
+batchSize = 128 -- size of mini-batches
 learningRate = 0.05 -- Learning rate used in AdaGrad
 
 initrounds = 5 -- Amount of intialization rounds in AdaGrad
 
-trsize = 50000 -- Size of training set
-tesize = 10000 -- Size of test set
+trsize = 50000-80 -- Size of training set
+tesize = 10000-16 -- Size of test set
 
 -- Loading data
 -- trainData is table with field 'data' which contains the data
 trainData, testData = loadMnist(trsize,tesize)
+
+trainData.data = trainData.data:cuda()
+testData.data = testData.data:cuda()
 
 -- Model Specific parameters
 filter_size = 5
@@ -16,33 +25,38 @@ filter_size_2 = 5
 stride = 2
 stride_2 = 2
 dim_hidden = 25
-input_size = 28 
-pad1 = 1 --NB new size must be divisible with filtersize
-pad2 = 2
-pad2_1 = 2
-pad2_2 = 1
-total_output_size = 1 * input_size ^ 2
+input_size = 28 --NB this is done later (line 129)
+pad1_1 = 1 --NB new size must be divisible with filtersize
+pad1_2 = 2
+pad2_1 = 1
+pad2_2 = 2
+colorchannels = 1
+total_output_size = colorchannels * input_size ^ 2
 feature_maps = 32
 feature_maps_2 = feature_maps*2
-
-hidden_dec = 50
-hidden_dec_2 = 100
+factor = 2
 
 map_size = 14
 map_size_2 = 7
-factor = input_size/map_size
-factor_2 = map_size/map_size_2
-colorchannels = 1
+
+hidden_dec = 40
+hidden_dec_2 = 80
+
+
 
 --layer1
 encoder = nn.Sequential()
-encoder:add(nn.SpatialZeroPaddingC(pad1,pad2,pad1,pad2))
-encoder:add(nn.SpatialConvolution(1,feature_maps,filter_size,filter_size,stride,stride))
-encoder:add(nn.Threshold(0,0))
+encoder:add(nn.Transpose({1,4},{1,3},{1,2}))
+encoder:add(nn.SpatialZeroPaddingCUDA(pad1_1,pad1_2,pad1_1,pad1_2))
+encoder:add(nn.SpatialConvolutionCUDA(colorchannels,feature_maps,filter_size,filter_size,stride,stride))
+encoder:add(nn.Threshold(0,1e-6))
+
 --layer2
-encoder:add(nn.SpatialZeroPaddingC(pad2_1,pad2_2,pad2_1,pad2_2))
-encoder:add(nn.SpatialConvolution(feature_maps,feature_maps_2,filter_size_2,filter_size_2,stride_2,stride_2))
-encoder:add(nn.Threshold(0,0))
+
+encoder:add(nn.SpatialZeroPaddingCUDA(pad2_1,pad2_2,pad2_1,pad2_2)) 
+encoder:add(nn.SpatialConvolutionCUDA(feature_maps,feature_maps_2,filter_size_2,filter_size_2,stride_2,stride_2))
+encoder:add(nn.Transpose({4,1},{4,2},{4,3}))
+encoder:add(nn.Threshold(0,1e-6))
 encoder:add(nn.Reshape(feature_maps_2 * map_size_2^2))
 
 local z = nn.ConcatTable()
@@ -59,9 +73,8 @@ decoder:add(nn.Reshape((map_size_2^2)*batchSize,feature_maps_2))
 decoder:add(nn.LinearCR(feature_maps_2,hidden_dec_2))
 decoder:add(nn.Threshold(0,1e-6))
 decoder:add(nn.LinearCR(hidden_dec_2,feature_maps*factor*factor)) --hidden_dec should be in order of: featuremaps * filtersize^2 / (16+factor^2)
-decoder:add(nn.Threshold(0,1e-6))
+decoder:add(nn.Threshold(0,1e-6)
 --layer1
-decoder:add(nn.Reshape((map_size^2)*batchSize,feature_maps))
 decoder:add(nn.LinearCR(feature_maps,hidden_dec))
 decoder:add(nn.Threshold(0,1e-6))
 decoder:add(nn.LinearCR(hidden_dec,colorchannels*factor*factor)) --hidden_dec should be in order of: featuremaps * filtersize^2 / (16+factor^2)
@@ -72,3 +85,7 @@ model = nn.Sequential()
 model:add(encoder)
 model:add(nn.Reparametrize(dim_hidden))
 model:add(decoder)
+model:add(nn.Copy('torch.CudaTensor','torch.DoubleTensor'))
+
+encoder:cuda()
+decoder:cuda()
