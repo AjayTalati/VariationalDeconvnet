8cea89b87d510bd9febfe7ca1de645b40294881e
y0ast <joost@joo.st>
2014-06-23 15:10:42 +0200

    put comment on top

--- results/MNIST/cuda/2-layer-14-14/config.lua
+++ results/MNIST/cuda/2-layer-14-14/config.lua
@@ -37,6 +37,7 @@ factor = 2
 map_size = 14
 map_size_2 = map_size
 
+--hidden_dec should be in order of: featuremaps * filtersize^2 / (16+factor^2)
 hidden_dec = 50
 
 
@@ -74,7 +75,7 @@ decoder:add(nn.Transpose({1,4}))
 decoder:add(nn.Reshape((map_size^2)*batchSize,feature_maps))
 decoder:add(nn.LinearCR(feature_maps,hidden_dec))
 decoder:add(nn.Threshold(0,1e-6))
-decoder:add(nn.LinearCR(hidden_dec,colorchannels*factor*factor)) --hidden_dec should be in order of: featuremaps * filtersize^2 / (16+factor^2)
+decoder:add(nn.LinearCR(hidden_dec,colorchannels*factor*factor))
 decoder:add(nn.Sigmoid())
 decoder:add(nn.Reshape(batchSize,total_output_size))
 
