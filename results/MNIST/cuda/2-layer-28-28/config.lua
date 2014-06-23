3025760755dbbebac6312de42ab867de0af7397f
OttoFabius <ottofabius@gmail.com>
2014-06-23 21:47:12 +0200

    28-28

--- results/MNIST/cuda/2-layer-28-28/config.lua
+++ results/MNIST/cuda/2-layer-28-28/config.lua
@@ -5,7 +5,7 @@ require 'SpatialZeroPaddingCUDA'
 cuda = true
 
 batchSize = 128 -- size of mini-batches
-learningRate = 0.02 -- Learning rate used in AdaGrad
+learningRate = 0.03 -- Learning rate used in AdaGrad
 
 initrounds = 10 -- Amount of intialization rounds in AdaGrad
 
@@ -26,24 +26,20 @@ input_size = 28
 pad1 = 2 --NB new size must be divisible with filtersize
 pad2 = 2
 total_output_size = 1 * input_size ^ 2
-feature_maps = 32
-feature_maps_2 = 64
+feature_maps = 16
+feature_maps_2 = 32
 colorchannels = 1
 
 map_size = 28^2
 --factor = input_size/ 16
 
 encoder = nn.Sequential()
-encoder:add(nn.Transpose({1,4},{1,3},{1,2}))
-encoder:add(nn.SpatialZeroPaddingCUDA(pad1,pad2,pad1,pad2))
-encoder:add(nn.SpatialConvolutionCUDA(colorchannels,feature_maps,filter_size,filter_size))
+encoder:add(nn.SpatialZeroPadding(pad1,pad2,pad1,pad2))
+encoder:add(nn.SpatialConvolution(colorchannels,feature_maps,filter_size,filter_size))
 
-encoder:add(nn.SpatialZeroPaddingCUDA(pad1,pad2,pad1,pad2))
-encoder:add(nn.SpatialConvolutionCUDA(feature_maps,feature_maps_2,filter_size,filter_size))
-
-encoder:add(nn.Transpose({4,1},{4,2},{4,3}))
+encoder:add(nn.SpatialZeroPadding(pad1,pad2,pad1,pad2))
+encoder:add(nn.SpatialConvolution(feature_maps,feature_maps_2,filter_size,filter_size))
 encoder:add(nn.Threshold(0,1e-6))
-
 encoder:add(nn.Reshape(feature_maps * map_size))
 
 local z = nn.ConcatTable()
@@ -57,13 +53,11 @@ decoder:add(nn.LinearCR(dim_hidden, feature_maps * map_size))
 decoder:add(nn.Threshold(0,1e-6))
 decoder:add(nn.Reshape(batchSize,feature_maps_2,input_size,input_size))
 
-decoder:add(nn.Transpose({1,4},{1,3},{1,2}))
-decoder:add(nn.SpatialZeroPaddingCUDA(pad1,pad2,pad1,pad2))
-decoder:add(nn.SpatialConvolutionCUDA(feature_maps_2,feature_maps,filter_size,filter_size))
+decoder:add(nn.SpatialZeroPadding(pad1,pad2,pad1,pad2))
+decoder:add(nn.SpatialConvolution(feature_maps_2,feature_maps,filter_size,filter_size))
 
-decoder:add(nn.SpatialZeroPaddingCUDA(pad1,pad2,pad1,pad2))
-decoder:add(nn.SpatialConvolutionCUDA(feature_maps,colorchannels,filter_size,filter_size))
-decoder:add(nn.Transpose({4,1},{4,2},{4,3}))
+decoder:add(nn.SpatialZeroPadding(pad1,pad2,pad1,pad2))
+decoder:add(nn.SpatialConvolution(feature_maps,colorchannels,filter_size,filter_size))
 
 decoder:add(nn.Sigmoid())
 decoder:add(nn.Reshape(batchSize,total_output_size))
