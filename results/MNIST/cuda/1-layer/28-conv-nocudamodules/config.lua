0ae059105216d6adda7460070a2831ec875e94a4
OttoFabius <ottofabius@gmail.com>
2014-07-01 19:15:05 +0200

    change lr

--- results/MNIST/cuda/1-layer/28-conv-nocudamodules/config.lua
+++ results/MNIST/cuda/1-layer/28-conv-nocudamodules/config.lua
@@ -7,7 +7,7 @@ cuda = true
 batchSize = 128 -- size of mini-batches
 learningRate = 0.02 -- Learning rate used in AdaGrad
 
-initrounds = 10 -- Amount of intialization rounds in AdaGrad
+initrounds = 20 -- Amount of intialization rounds in AdaGrad
 
 trsize = 50000-80 -- Size of training set
 tesize = 10000-16 -- Size of test set
