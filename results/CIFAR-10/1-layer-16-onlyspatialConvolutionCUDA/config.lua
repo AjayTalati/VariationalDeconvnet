dca76d5d86f9ec9cdbb80bc248b9e3846c1a0bf3
OttoFabius <ottofabius@gmail.com>
2014-07-02 13:23:28 +0200

    configs cifar

--- results/CIFAR-10/1-layer-16-onlyspatialConvolutionCUDA/config.lua
+++ results/CIFAR-10/1-layer-16-onlyspatialConvolutionCUDA/config.lua
@@ -1,6 +1,6 @@
 --One layer deconvnet with padding
-cuda = false	
 require 'GaussianCriterion'
+cuda = true
 if cuda then
 	require 'cutorch'
 	require 'cunn'
@@ -13,7 +13,7 @@ continuous = true
 batchSize = 128 -- size of mini-batches
 learningRate = 0.03 -- Learning rate used in AdaGrad
 
-initrounds = 10 -- Amount of intialization rounds in AdaGrad
+initrounds = 20 -- Amount of intialization rounds in AdaGrad
 
 trsize = 50000-80 -- Size of training set
 tesize = 10000-16 -- Size of test set
@@ -21,6 +21,7 @@ tesize = 10000-16 -- Size of test set
 -- Loading data
 -- trainData is table with field 'data' which contains the data
 trainData, testData = loadCifar(trsize,tesize,false)
+
 if cuda then
 	trainData.data = trainData.data:cuda()
 	testData.data = testData.data:cuda()
@@ -29,64 +30,54 @@ end
 -- Model Specific parameters
 filter_size = 5
 stride = 2
-dim_hidden = 25
+dim_hidden = 100
 input_size = 32 --NB this is done later (line 129)
 pad1 = 2 --NB new size must be divisible with filtersize
 pad2 = 2
 colorchannels = 3
 total_output_size = colorchannels * input_size ^ 2
-feature_maps = 16
-feature_maps_2 = 32
+feature_maps = 32
 
 hidden_dec = 50
-hidden_dec_2 = 50
 
 map_size = 16
-map_size_2 = 8
 factor = stride
 
 
 encoder = nn.Sequential()
---L2
 encoder:add(nn.SpatialZeroPadding(pad1,pad2,pad1,pad2))
+
+encoder:add(nn.Transpose({1,4},{1,3},{1,2}))
+encoder:add(nn.SpatialConvolutionCUDA(colorchannels,feature_maps,filter_size,filter_size))
+encoder:add(nn.Transpose({4,1},{4,2},{4,3}))
+
 encoder:add(nn.SpatialConvolution(colorchannels,feature_maps,filter_size,filter_size))
 encoder:add(nn.SpatialMaxPooling(2,2,2,2))
-encoder:add(nn.Threshold(0,1e-6))
---L2
-encoder:add(nn.SpatialZeroPadding(pad1,pad2,pad1,pad2))
-encoder:add(nn.SpatialConvolution(feature_maps,feature_maps_2,filter_size,filter_size))
-encoder:add(nn.SpatialMaxPooling(2,2,2,2))
+
 encoder:add(nn.Threshold(0,1e-6))
 
-encoder:add(nn.Reshape(feature_maps_2 * map_size_2 * map_size_2))
+encoder:add(nn.Reshape(feature_maps * map_size * map_size))
 
 local z = nn.ConcatTable()
-z:add(nn.LinearCR(feature_maps_2 * map_size_2 * map_size_2, dim_hidden))
-z:add(nn.LinearCR(feature_maps_2 * map_size_2 * map_size_2, dim_hidden))
+z:add(nn.LinearCR(feature_maps * map_size * map_size, dim_hidden))
+z:add(nn.LinearCR(feature_maps * map_size * map_size, dim_hidden))
 
 encoder:add(z)
 
 local decoder = nn.Sequential()
-decoder:add(nn.LinearCR(dim_hidden, feature_maps_2 * map_size_2 * map_size_2))
+decoder:add(nn.LinearCR(dim_hidden, feature_maps * map_size * map_size))
 decoder:add(nn.Threshold(0,1e-6))
 
-decoder:add(nn.Reshape(feature_maps_2,map_size_2,map_size_2))
+decoder:add(nn.Reshape(feature_maps,map_size,map_size))
 decoder:add(nn.Transpose({2,3},{3,4}))
-decoder:add(nn.Reshape(map_size_2*map_size_2*batchSize,feature_maps_2))
 
---first decoding layer
-decoder:add(nn.LinearCR(feature_maps_2,hidden_dec_2))
-decoder:add(nn.Threshold(0,1e-6))
-decoder:add(nn.LinearCR(hidden_dec_2,feature_maps*factor*factor))
-decoder:add(nn.Threshold(0,1e-6))
-
---second decoding layer
-decoder:add(nn.LinearCR(feature_maps*factor*factor,hidden_dec))
+decoder:add(nn.Reshape(map_size*map_size*batchSize,feature_maps))
+decoder:add(nn.LinearCR(feature_maps,hidden_dec))
 decoder:add(nn.Threshold(0,1e-6))
 
 decoder2 = nn.ConcatTable()
-decoder2:add(nn.LinearCR(hidden_dec, colorchannels*factor*factor*factor*factor))
-decoder2:add(nn.LinearCR(hidden_dec, colorchannels*factor*factor*factor*factor))
+decoder2:add(nn.LinearCR(hidden_dec, colorchannels*factor*factor))
+decoder2:add(nn.LinearCR(hidden_dec, colorchannels*factor*factor))
 
 decoder3 = nn.ParallelTable()
 decoder3:add(nn.Sigmoid())
@@ -102,8 +93,6 @@ if cuda then
 	decoder5:add(nn.Copy('torch.CudaTensor','torch.DoubleTensor'))
 end
 
-
-
 model = nn.Sequential()
 model:add(encoder)
 model:add(nn.Reparametrize(dim_hidden))
@@ -111,6 +100,7 @@ model:add(decoder)
 model:add(decoder2)
 model:add(decoder3)
 model:add(decoder4)
+
 
 if cuda then
 	model:add(decoder5)
@@ -120,4 +110,3 @@ if cuda then
 	decoder3:cuda()
 	decoder4:cuda()
 end
-
